fc5f73ed7b25deed0022f8550ab9863271c8ba68
Aymane Harmaz <aymane.harmaz1@gmail.com>
2024-05-28 21:31:41 +0100

    adding data to the persons table

---
+++ src/main/resources/db/migrations/V2__inserting_rows_into_persons_table.sql
@@ -0,0 +1,3 @@
+insert into persons (id, first_name, last_name, age, email, country, city) values (1, 'aymane', 'harmaz', 23, 'aymane.harmaz@gmail.com', 'Morocco', 'Casablanca');
+insert into persons (id, first_name, last_name, age, email, country, city) values (2, 'Clementine', 'Hernandez', 23, 'clementine.hernandez@gmail.com', 'France', 'Paris');
+insert into persons (id, first_name, last_name, age, email, country, city) values (3, 'Fabio', 'almiras', 26, 'fabio.almiras@gmail.com', 'Mexico', 'Mexico');
