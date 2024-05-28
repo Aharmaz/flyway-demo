aecfcfe2656767bb7eddad969bd2bdd89106996a
Aymane Harmaz <aymane.harmaz1@gmail.com>
2024-05-28 20:26:16 +0100

    setting up a spring boot project

---
+++ src/main/java/ma/demo/flyway/FlywayDemoApplication.java
@@ -0,0 +1,11 @@
+package ma.demo.flyway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlywayDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FlywayDemoApplication.class);
+    }
+}
